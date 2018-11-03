aa6de3e74f9c29bdb2d0373b99734a7814231cff
Max Ivanov <maxlvan@yandex.ru>
2018-11-04 00:29:52 +0300

    Lesson 1: App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import {TodoDataService} from './todo-data.service';
+import {Todo} from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'ng-app';
+  newTodo: Todo = new Todo();
+
+  constructor(private todoDataService: TodoDataService) {
+  }
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+
 }
